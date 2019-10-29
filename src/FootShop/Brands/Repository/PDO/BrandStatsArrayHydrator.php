da5bf435fb3444f8376e83375ddca940850bd3dd
Lukáš Klika <info@lukasklika.cz>
2019-10-29 16:31:12 +0100

    BrandStatsArrayHydrator bugfix

--- src/FootShop/Brands/Repository/PDO/BrandStatsArrayHydrator.php
+++ src/FootShop/Brands/Repository/PDO/BrandStatsArrayHydrator.php
@@ -15,7 +15,7 @@ class BrandStatsArrayHydrator
         $stats->setName((string)$data['name']);
         $stats->setQuantity(array_key_exists('quantity', $data) ? (int)$data['quantity'] : 0);
         $stats->setReserved(array_key_exists('reserved', $data) ? (int)$data['reserved'] : 0);
-        $stats->setPriceQuantity(array_key_exists('priceQuantity', $data) ? (int)$data['priceQuantity'] : 0);
-        $stats->setPriceReserved(array_key_exists('priceReserved', $data) ? (int)$data['priceReserved'] : 0);
+        $stats->setPriceQuantity(array_key_exists('price_quantity', $data) ? (int)$data['price_quantity'] : 0);
+        $stats->setPriceReserved(array_key_exists('price_reserved', $data) ? (int)$data['price_reserved'] : 0);
     }
 }
